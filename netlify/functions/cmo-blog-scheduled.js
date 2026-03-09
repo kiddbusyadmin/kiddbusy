00e973622849832010114fded8ce9537ef0157e3
Harold T. Respass <Harold@Harolds-MacBook-Pro.local>
2026-03-09 19:20:06 -0400

    Separate scheduled CMO runner from manual API endpoint

--- netlify/functions/cmo-blog-scheduled.js
+++ netlify/functions/cmo-blog-scheduled.js
@@ -1,7 +1,5 @@
-exports.handler = async function handler() {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ success: true, message: 'Cron trigger now handled by cmo-blog-run-background schedule.' })
-  };
+const { runCmoBlog } = require('./_cmo-blog-core');
+
+exports.handler = async function handler(event) {
+  return runCmoBlog(event);
 };
